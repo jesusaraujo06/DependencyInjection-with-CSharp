2161bc65e9228e4f033cf83ab756af941bff4284
Jesus Araujo <jesus.araujo@coolechera.com>
2023-01-19 23:50:57 -0500

    Reestructurar proyectos/carpetas

---
+++ NoDependencyInjection/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using NoDependencyInjection.Services;
+
+var customerService = new CustomerService();
+var CommunicationService = new CommunicationService();
+
+var customers = customerService.GetCustomers();
+
+var message = "Message to broadcast to all customers";
+
+foreach (var customer in customers)
+{
+    CommunicationService.SendMessage(customer, message);
+}
